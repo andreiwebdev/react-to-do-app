704d7783b73c5f8aa1c503bb1650779df0296ced
Andrei <dandrei.web@gmail.com>
2023-02-08 20:00:45 +0200

    added dynamic scroll

--- src/components/Layout/TodosContainer.jsx
+++ src/components/Layout/TodosContainer.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const TodosContainer = (props) => {
   return (
-    <div className="bg-slate-200 w-10/12 h-[90vh] rounded-2xl text-black p-7 text-center ">
+    <div className="bg-slate-200 w-10/12 h-[90vh] rounded-2xl text-black p-7 text-center relative ">
       {props.children}
     </div>
-  )
-}
+  );
+};
 
-export default TodosContainer
+export default TodosContainer;
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TodoItem from "../TodoItem/TodoItem";
 import useTodo from "../../hooks/useTodo";
 
 const TodoList = () => {
   const { todos } = useTodo();
+  const [todosScroll, setTodosScroll] = useState(
+    "max-h-[fill] w-full h-full absolute pr-[45px]"
+  );
+
+  useEffect(() => {
+    if (todos.length >= 2) {
+      setTodosScroll(
+        "max-h-[fill] w-full h-full absolute overflow-y-scroll pr-[30px]"
+      );
+    } else {
+      setTodosScroll("max-h-[fill] w-full h-full absolute pr-[45px]");
+    }
+  }, [todos]);
 
   const todosItems = (
-    <ul>
+    <ul className={todosScroll}>
       {todos.map((todo) => (
         <TodoItem key={todo.id} id={todo.id} todo={todo.text} />
       ))}
